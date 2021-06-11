2435f51c1e9ae46b99db5b5f79682a0abf92cdf1
JenniferSilva25 <silvajennifer2509@gmail.com>
2021-06-11 19:55:08 -0300

    Adicionando novas funcionalidas
    Botão de pausa na hora da fala
    Input do tipo range para controlar a valocidade da fala

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
+const textInput = document.querySelector('#text');
+const speedInput = document.querySelector('#speed');
+const ratePosition = document.querySelector('#rate');
+const buttonPause = document.querySelector('#button-pause');
+
 const utterance = new SpeechSynthesisUtterance();
 
-utterance.lang = "pt-BR";
-//utterance.rate = 2;
+speedInput.addEventListener('click', () => {
+  const rateInput = speedInput.value;
+  ratePosition.innerHTML = rateInput;
+  console.log(rateInput);
 
-function speak() {
-	speechSynthesis.speak(utterance);
+})
+
+buttonPause.addEventListener('click', pauseText)
+
+
+// Falar em portugues.
+utterance.lang = "pt-BR";
+
+// Comecar a falar
+const speak = (event) => {
+  if (speechSynthesis.paused || speechSynthesis.speaking) {
+    return speechSynthesis.resume();
+  }
+  utterance.rate = speedInput.value || 1;
+  speechSynthesis.speak(utterance);
 }
 
-function stop() {
+// Parar de falar
+const stop = () => {
   speechSynthesis.cancel();
   console.log("aqui...");
 }
 
-function setText(event) {
-    utterance.text = event.target.innerText;
-    console.log("aqui");
+// pausa a fala do texto
+function pauseText() {
+  if (speechSynthesis.speaking) speechSynthesis.pause();
+}
+
+const setText = (event) => {
+  utterance.text = event.target.innerText;
+
 }
